4d9d4103e8517f3fcb1d4c704bae70be191989d1
HarshCh16 <harshchauhanhc161005@gmail.com>
2025-04-27 12:15:41 +0530

    Added the tip calculator

--- DAY_1/main.py
+++
@@ -1,4 +0,0 @@
-print("BAND NAME GENERATOR\n\n")
-city = input("Whats the name of the city you grew up in?\n")
-pet = input("What's your pet's name?\n")
-print("\n\nYour Band name could be " + city + " " + pet)
---
+++ DAY_2/main.py
@@ -0,0 +1,9 @@
+print("WELCOME TO THE TIP CALCULATOR!")
+total_bill = input("\nWhat was the total bill? $")
+tip_percent = input("\nHow much tip would you like to give? 10, 12 or 15? ")
+no_of_people = input("\nHow many people to split the bill? ")
+tip = (float(tip_percent) / 100) * float(total_bill)
+total_amount = float(total_bill) + tip
+share_per_person = total_amount / float(no_of_people)
+rounded_off_amount = round(share_per_person , 2)
+print(f"\nEach person should pay {rounded_off_amount}")
